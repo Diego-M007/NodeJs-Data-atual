e293ae2181e174323a1f9da62fa7ae60626eada5
Diego Miranda <125681791+Diego-M007@users.noreply.github.com>
2023-09-20 12:49:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+
+	var dataAtual = new Date();
+	var dataFormatada = dataAtual.toLocaleDateString('pt-br');
+
+	res.end('<h1 style="font-size: 24px;"> Diego Miranda de Oliveira</h1>\n<p>Data atual: ' + dataFormatada + '</p>');
+}).listen(8004);
